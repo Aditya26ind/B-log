da8e3441d2f8056459a7d7fda94ab13a40bb8a95
ADITYA SALABH <68841009+Aditya26ind@users.noreply.github.com>
2024-07-14 17:35:44 +0530

    render configuration

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
 
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
 
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+# Convert static asset files
+python manage.py collectstatic --no-input
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+# Apply any outstanding database migrations
+python manage.py migrate
